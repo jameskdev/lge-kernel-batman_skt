fbdf5066541c9171aebb145aaa2c31c2a44faa7b
Lucille Sylvester <lsylvest@codeaurora.org>
2012-04-11 12:17:38 -0600

    msm: kgsl: Resolve conflicts in state changes
    Only check requested state once, and fully transition to that
    specific state.  This will resolve any conflicts arising from
    changes to the requested_state as the state transition occurs.
    
    Signed-off-by: Lucille Sylvester <lsylvest@codeaurora.org>
    (cherry picked from commit e4a7c1a365ea32aebfe3995e13fc63ca8c2916c5)
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_pwrctrl.c
    
    Change-Id: I6940a054bd4f1e9e3d9e078f0fe57a2afd071102
    Signed-off-by: Zhoulu Luo <zluo@codeaurora.org>
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_pwrctrl.c

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -335,10 +335,8 @@ static void kgsl_pwrctrl_busy_time(struct kgsl_device *device, bool on_time)
 	do_gettimeofday(&(b->start));
 }
 
-//void kgsl_pwrctrl_clk(struct kgsl_device *device, int state)
 void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
-	int requested_state)
-
+					  int requested_state)
 {
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 	int i = 0;
@@ -350,7 +348,6 @@ void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
 				if (pwr->grp_clks[i])
 					clk_disable(pwr->grp_clks[i]);
 			if ((pwr->pwrlevels[0].gpu_freq > 0) &&
-				//(device->requested_state != KGSL_STATE_NAP))
 				(requested_state != KGSL_STATE_NAP))
 				clk_set_rate(pwr->grp_clks[0],
 					pwr->pwrlevels[pwr->num_pwrlevels - 1].
@@ -703,8 +700,6 @@ _nap(struct kgsl_device *device)
 			return -EBUSY;
 		}
 		kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_OFF);
-		//kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_OFF);
-		//kgsl_pwrctrl_set_state(device, device->requested_state);
 		kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_OFF, KGSL_STATE_NAP);
 		kgsl_pwrctrl_set_state(device, KGSL_STATE_NAP);
 		if (device->idle_wakelock.name)
@@ -748,7 +743,6 @@ _sleep(struct kgsl_device *device)
 				pwr->pwrlevels[pwr->num_pwrlevels - 1].
 				gpu_freq);
 		_sleep_accounting(device);
-		//kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_OFF);
 		kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_OFF, KGSL_STATE_SLEEP);
 		kgsl_pwrctrl_set_state(device, KGSL_STATE_SLEEP);
 //		if (device->idle_wakelock.name)
@@ -854,7 +848,6 @@ void kgsl_pwrctrl_wake(struct kgsl_device *device)
 		/* fall through */
 	case KGSL_STATE_NAP:
 		/* Turn on the core clocks */
-		//kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_ON);
 		kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_ON, KGSL_STATE_ACTIVE);
 		/* Enable state before turning on irq */
 		kgsl_pwrctrl_set_state(device, KGSL_STATE_ACTIVE);
@@ -884,7 +877,6 @@ void kgsl_pwrctrl_enable(struct kgsl_device *device)
 {
 	/* Order pwrrail/clk sequence based upon platform */
 	kgsl_pwrctrl_pwrrail(device, KGSL_PWRFLAGS_ON);
-	//kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_ON);
 	kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_ON, KGSL_STATE_ACTIVE);
 	kgsl_pwrctrl_axi(device, KGSL_PWRFLAGS_ON);
 }
@@ -894,7 +886,6 @@ void kgsl_pwrctrl_disable(struct kgsl_device *device)
 {
 	/* Order pwrrail/clk sequence based upon platform */
 	kgsl_pwrctrl_axi(device, KGSL_PWRFLAGS_OFF);
-	//kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_OFF);
 	kgsl_pwrctrl_clk(device, KGSL_PWRFLAGS_OFF, KGSL_STATE_SLEEP);
 	kgsl_pwrctrl_pwrrail(device, KGSL_PWRFLAGS_OFF);
 }
